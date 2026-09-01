170fac5f296ada64aa9d2446c9d550b8e1efdef2
Awu <hanlin.wu.2025@computing.smu.edu.sg>
2026-09-01 16:15:26 +0800

    Lab1 1st time submit by Hanlin

--- NumbersArray.java
+++ NumbersArray.java
@@ -20,6 +20,86 @@ public class NumbersArray {
     }
 
     // Write your methods here
+
+    /*
+        findMax
+        returns the maximum value in the array
+    */
+    // Start
     
+    public static String findMax(Integer[] input) {
+        int maxValue = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] > maxValue) {
+                maxValue = input[i];
+            }
+        }
+
+        return Integer.toString(maxValue);
+    }
+    // End
+
+
+    /*
+        findDuplicates
+        returns an array (Integer[]) containing the duplicate values in the array
+
+    */
+    // Start
+    
+    public static Integer[] findDuplicates(Integer[] input) {
+        HashMap<Integer, Integer> resultMap = new HashMap<>();
+
+        for (int i = 0; i < input.length; i++) {
+            if (!resultMap.containsKey(input[i])) {
+                resultMap.put(input[i], 1);
+            } else {
+                resultMap.put(input[i], resultMap.get(input[i]) + 1);
+            }
+        }
+
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : resultMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                tempList.add(entry.getKey());
+            }
+        }
+
+        Integer[] resultArray = new Integer[tempList.size()];
+        return tempList.toArray(resultArray);
+    }
+    // End
+
+
+    /*
+        findUnique
+        returns an array (Integer[]) containing the unique values in the array
+
+    */
+    // Start
+    
+    public static Integer[] findUnique(Integer[] input) {
+        HashMap<Integer, Integer> resultMap = new HashMap<>();
+
+        for (int i = 0; i < input.length; i++) {
+            if (!resultMap.containsKey(input[i])) {
+                resultMap.put(input[i], 1);
+            } else {
+                resultMap.put(input[i], resultMap.get(input[i]) + 1);
+            }
+        }
+
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : resultMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                tempList.add(entry.getKey());
+            }
+        }
+
+        Integer[] resultArray = new Integer[tempList.size()];
+        return tempList.toArray(resultArray);
+    }
+    // End
 }
 
